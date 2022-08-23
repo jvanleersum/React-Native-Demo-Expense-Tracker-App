1a48c490de0cab56b9a8745441f861fa437078b0
Judith van Leersum <judithvleersum@gmail.com>
2022-08-23 11:49:04 +0200

    Getting recent expenses from context

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,43 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
-import ExpensesOutput from "../components/Expenses/ExpensesOutput";
+import { View, StyleSheet } from "react-native";
+import { useContext } from "react";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "New pair of shoes",
-    amount: 28.89,
-    date: new Date("2022-01-29"),
-  },
-  {
-    id: "e2",
-    title: "10 books",
-    amount: 50.92,
-    date: new Date("2022-03-29"),
-  },
-  {
-    id: "e3",
-    title: "Great new laptop",
-    amount: 291.99,
-    date: new Date("2022-06-29"),
-  },
-  {
-    id: "e4",
-    title: "Glass of water",
-    amount: 1.59,
-    date: new Date("2022-08-11"),
-  },
-  {
-    id: "e5",
-    title: "A few beers",
-    amount: 7.99,
-    date: new Date("2022-08-20"),
-  },
-];
+import ExpensesContext from "../store/expenses-context";
+import ExpensesOutput from "../components/Expenses/ExpensesOutput";
+import { getDateMinusDays } from "../utils/date";
 
 const RecentExpenses = () => {
+  const expCtx = useContext(ExpensesContext)
+  const recentExpenses = expCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const sevenDaysAgo = getDateMinusDays(today, 7);
+    return expense.date > sevenDaysAgo
+  })
+
   return (
     <View style={styles.container}>
-      <ExpensesOutput items={DUMMY_EXPENSES} expensePeriod="Last 7 days"/>
+      <ExpensesOutput items={recentExpenses} expensePeriod="Last 7 days"/>
     </View>
   );
 };
