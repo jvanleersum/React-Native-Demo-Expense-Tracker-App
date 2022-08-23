912bc17289eb9a6cca40faf331b6e180d550b64a
Judith van Leersum <judithvleersum@gmail.com>
2022-08-23 11:06:04 +0200

    Added cancel and submit buttons and logic to go back

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import Colors from "../constants/colors";
 
@@ -7,18 +8,31 @@ const ManageExpense = ({ route, navigation }) => {
   const expenseId = route.params?.expenseId;
   const isEditing = !!expenseId;
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     navigation.setOptions({
       title: isEditing ? "Edit Expense" : "Add New Expense",
     });
   }, [navigation, isEditing]);
 
-  const deleteExpenseHandler = () => {}
+  const deleteExpenseHandler = () => {
+    navigation.goBack();
+  }
+
+  const cancelHandler = () => {
+    navigation.goBack();
+  }
+
+  const confirmHandler = () => {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Button mode="flat" style={styles.button} onPress={cancelHandler}>Cancel</Button>
+        <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+      </View>
       <View style={styles.deleteContainer}>
-
       {isEditing && <IconButton icon="trash" size={36} color={Colors.error500} onPress={deleteExpenseHandler}/>}
       </View>
     </View>
@@ -31,6 +45,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: 16
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+  button: {
+    minWidth: 120, 
+    marginHorizontal: 8
   },
   deleteContainer: {
     margin: 16,
